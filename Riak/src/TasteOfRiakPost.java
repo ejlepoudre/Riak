9c356c0e0b76cec86c3828cb369365382b8ae341
Eric LePoudre <ejlepoudre@gmail.com>
2014-10-29 20:33:25 -0700

    add riak

---
+++ Riak/src/TasteOfRiakPost.java
@@ -0,0 +1,106 @@
+import com.basho.riak.client.*;
+import com.basho.riak.client.bucket.*;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class TasteOfRiakPost
+{
+	
+	 
+    	public static void main(String[] args) throws Exception {
+     
+   
+    		System.out.println("Testing 1 - Send Http GET request");
+    		sendGet();
+     
+    		System.out.println("\nTesting 2 - Send Http POST request");
+    		sendPost();
+    		
+        }
+
+ 
+	// HTTP GET request
+	private static void sendGet() throws Exception {
+ 
+		String url = "http://localhost:8098/buckets/tasteofriak/keys/asap";
+		String USER_AGENT = "Mozilla/5.0";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		// optional default is GET
+		con.setRequestMethod("GET");
+ 
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+ 
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		System.out.println(response.toString());
+ 
+	}
+ 
+	// HTTP POST request
+	private static void sendPost() throws Exception {
+ 
+		String url = "http://localhost:8098/buckets/tasteofriak/keys/a2";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		String USER_AGENT = "Mozilla/5.0";
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		con.setRequestProperty("Content-Type", "text/plain");
+ 
+		String urlParameters = "23";
+ 
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+ 
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		System.out.println(response.toString());
+ 
+	}
+}
